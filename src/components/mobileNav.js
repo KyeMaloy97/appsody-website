ca41550ea8f08dd8e968b4de23bfdc3c54e2fe9b
Kye Maloy <kye.maloy@ibm.com>
2019-10-18 10:07:22 +0100

    initial changes

---
+++ src/components/mobileNav.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import { Link } from "gatsby"
+
+import styles from "./mobileNav.module.css"
+
+const MobileNav = () => {
+
+  return (
+    <nav className={styles.mobileNav}> 
+      <Link className={styles.navLink} to="/">Home</Link>
+
+      <Link className={styles.navLink} to="/docs">Docs</Link> 
+
+      <a className={styles.navLink} href="https://medium.com/appsody" target="_blank" rel="noopener noreferrer">Tutorials</a>
+
+    </nav>
+  )
+}
+
+export default MobileNav

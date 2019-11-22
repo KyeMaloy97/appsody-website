a351eccd57effef90c65c7897a1183bd00599d9d
Kye Maloy <kye.maloy@ibm.com>
2019-11-22 11:20:14 +0000

    initial changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,7 @@ import React from "react";
 const Footer = () => {
     return (
         <footer>
+            <img class="footer-image" src="https://i.ytimg.com/vi/CPw06Ag-Wfs/maxresdefault.jpg" alt="favicon" width="200px"></img>
             <ul>
                 <li><a href="https://github.com/appsody/website/blob/master/LICENSE">License</a></li>
                 <li><a href="https://github.com/appsody/website/blob/master/NOTICE.md">Notice</a></li>
